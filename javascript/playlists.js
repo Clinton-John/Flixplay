9315bc67c9f91bac486df1617585ff393c1c255a
clintonjohn-cmd <johnouko@kabarak.ac.ke>
2023-08-06 22:29:53 +0300

    adding songs in the palylists

--- javascript/playlists.js
+++ javascript/playlists.js
@@ -5,13 +5,20 @@ let LibraryManager = {
         this.toggleLastSection();
     },
     toggleLastSection: function() {
+        let getSongNumber = document.querySelector("#song-number");
+        let getPlayingIcon = document.querySelector(".playing-icon");
+
         let getPlaylistName = document.querySelector(".playlist-name");
         let getLastSection = document.querySelector(".last-section");
         let getRightBody = document.querySelector(".right-body");
 
-        getLastSection.style.width = "20%";
+        getPlaylistName.style.color = "green";
+        getLastSection.style.width = "25%";
+        getLastSection.style.visibility = "visible";
+        getLastSection.style.marginLeft = "10px";
         getRightBody.style.width = "50%";
-        getPlaylistName.style.color = "red";
+        getPlayingIcon.style.visibility = "visible";
+
 
     },
     displaySongs: function() {
